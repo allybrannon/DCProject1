fd0461630ff2c7e35a196e6be3eca46dea72d0ef
Patrick Furtak <patrick@patrickfurtak.com>
2020-02-17 13:50:35 -0500

    refactored inline JS, adjusted darkmode toggle to stick on mobile

--- javascript/darkMode.js
+++ javascript/darkMode.js
@@ -1,6 +1,13 @@
+const moonBtn = document.querySelector('.moonBtn');
+
 function darkMode() {
   const bodyElement = document.body;
   const cardElement = document.querySelector('.card');
   cardElement.classList.toggle('darkMode');
   bodyElement.classList.toggle('darkMode');
 }
+
+moonBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+  darkMode();
+});
--- javascript/konami.js
+++ javascript/konami.js
@@ -3,7 +3,7 @@ const konamiCode = document.querySelector('.konamiCode');
 
 if (window.addEventListener) {
   var state = 0,
-    konami = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]; //up, up, down, down, left, right, left, right, b,
+    konami = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]; //up, up, down, down, left, right, left, right, b, a.
   window.addEventListener(
     'keydown',
     function(e) {
