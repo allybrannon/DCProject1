bfba4d1e990fa78c058a41193f5d1343c8b7c8e0
Patrick Furtak <patrick@patrickfurtak.com>
2020-02-18 10:15:30 -0500

    added darkmode button icon change

--- javascript/darkMode.js
+++ javascript/darkMode.js
@@ -7,7 +7,18 @@ function darkMode() {
   bodyElement.classList.toggle('darkMode');
 }
 
+function buttonChange() {
+  const bodyElement = document.body;
+  const btnImage = document.querySelector('.darkIcon');
+  if (bodyElement.className == 'darkMode') {
+    btnImage.src = '/images/icons8-summer-50.png';
+  } else if (bodyElement.className !== 'darkMode') {
+    btnImage.src = '/images/icons8-moon-symbol-30.png';
+  }
+}
+
 moonBtn.addEventListener('click', function(e) {
   e.preventDefault();
   darkMode();
+  buttonChange();
 });
