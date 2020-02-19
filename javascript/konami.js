1ec569a3bc0513b72c6b9d8596af87d11c09caff
Patrick Furtak <patrick@patrickfurtak.com>
2020-02-19 12:23:59 -0500

    fixed event listener bug, removed konami script load from landing page

--- javascript/konami.js
+++ javascript/konami.js
@@ -1,16 +1,11 @@
-//Konami code
 const konamiCode = document.querySelector('.konamiCode');
 
 if (window.addEventListener) {
   var state = 0,
-    konami = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-  window.addEventListener(
-    'keydown',
-    function(e) {
-      if (e.keyCode == konami[state]) state++;
-      else state = 0;
-      if (state == 10) konamiCode.style.opacity = '1';
-    },
-    true
-  );
+    konami = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]; //up, up, down, down, left, right, left, right, b, a.
+  window.addEventListener('keydown', e => {
+    if (e.keyCode == konami[state]) state++;
+    else state = 0;
+    if (state == 10) konamiCode.style.opacity = '1';
+  });
 }
