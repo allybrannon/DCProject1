db98d24d6dcd54145af224e036a03a9f1dd670f5
Patrick Furtak <patrick@patrickfurtak.com>
2020-02-19 09:21:12 -0500

    final commit before final merge

--- javascript/konami.js
+++ javascript/konami.js
@@ -4,13 +4,9 @@ const konamiCode = document.querySelector('.konamiCode');
 if (window.addEventListener) {
   var state = 0,
     konami = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]; //up, up, down, down, left, right, left, right, b, a.
-  window.addEventListener(
-    'keydown',
-    function(e) {
-      if (e.keyCode == konami[state]) state++;
-      else state = 0;
-      if (state == 10) konamiCode.style.opacity = '1';
-    },
-    true
-  );
+  window.addEventListener('keydown', function(e) {
+    if (e.keyCode == konami[state]) state++;
+    else state = 0;
+    if (state == 10) konamiCode.style.opacity = '1';
+  });
 }
